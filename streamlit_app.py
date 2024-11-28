47cb67f787348118ba7d945865c20179b6e38d9f
Billionaire-cmd <rabbigyimah7@gmail.com>
2024-11-28 19:16:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ def calculate_predictions():
 
     # Display results for Halftime predictions
     st.subheader("Most Likely Half-Time Scorelines:")
-    for scoreline in ht_results[:5]:  # Display top 5 HT scorelines
+    for scoreline in ht_results[:7]:  # Display top 5 HT scorelines
         home, away, prob = scoreline
         adjusted_prob = adjust_for_over_2_5_goals(over_2_5_odds, prob)
         st.write(f"HT {home}-{away} with Poisson Probability: {prob * 100:.2f}%, Adjusted for Over 2.5: {adjusted_prob * 100:.2f}%")
